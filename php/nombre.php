39a993ed6d563031a26813b8b41190a6a868e58c
Fabian Gonzalez Torres <fabian@MacBook-Air-de-Fabian.local>
2024-09-04 21:31:33 -0600

    Tablas y php

---
+++ php/nombre.php
@@ -0,0 +1,11 @@
+<html> 
+  <head> 
+    <meta charset="utf-8" />
+    <title>Nombre</title> 
+  </head>
+  <body>
+    <h1>Tu nombre es: <?php echo " ".$_GET['nombre']; ?> </h1> 
+
+      
+  </body>
+</html>
